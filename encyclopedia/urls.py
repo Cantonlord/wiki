345ac4b7e524b9aa0164d0f2d751903f2102915b
Alwyn Lee <alwynleee@gmail.com>
2022-04-04 22:22:55 +0800

    Update random url

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('create', views.create, name='create'),
     path('<str:title>/edit', views.edit, name='edit'),
-    
+    path('<str:title>/save', views.save, name='save'),
+    path('random', views.random_page, name='random')
     # re_path(r'(?P<title>\w+)/$', views.entry, name='entry'),
 ]
